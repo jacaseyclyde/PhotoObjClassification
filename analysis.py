8b8f6dac6879407d845c454e1e71faf01131d233
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2018-12-07 14:53:54 -0800

    Removed LDA data reduction to lighten testing time. May later add back in, but not as a seperate set of tests.

--- analysis.py
+++ analysis.py
@@ -17,6 +17,7 @@ import itertools
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+from sklearn.metrics import confusion_matrix, make_scorer
 from sklearn.model_selection import train_test_split, GridSearchCV
 
 from sklearn import preprocessing
@@ -59,7 +60,7 @@ def load_data():
     # but because each row represents a seperate observation, it's ok
     # and we don't need to filter for them
     data = pd.read_csv(os.path.join(os.path.dirname(__file__),
-                                    'data', 'sdss.csv'), nrows=1000)
+                                    'data', 'sdss.csv'))
 
     # Get rid of features that don't represent physical data or
     # something directly derived from it (i.e., not a fit).
@@ -67,7 +68,7 @@ def load_data():
     # classification
     data = data.drop(labels=[
                              'specObjID',
-                             'mjd', #
+                             'mjd', # Date
                              'plate', # ID
                              'tile', # ID
                              'fiberID', # ID
@@ -76,30 +77,30 @@ def load_data():
                              'zWarning',
 #                             'ra', #
 #                             'dec', #
-#                             'cx',
-#                             'cy',
-#                             'cz',
+#                             'cx', #
+#                             'cy', #
+#                             'cz', #
                              'htmID',
-#                             'sciencePrimary',
-#                             'legacyPrimary',
-#                             'seguePrimary',
-#                             'segue1Primary',
-#                             'segue2Primary',
-#                             'bossPrimary',
-#                             'sdssPrimary',
+#                             'sciencePrimary', #
+#                             'legacyPrimary', #
+#                             'seguePrimary', #
+#                             'segue1Primary', #
+#                             'segue2Primary', #
+#                             'bossPrimary', #
+#                             'sdssPrimary', #
                              'survey',
                              'programname',
                              'legacy_target1',
                              'legacy_target2',
-#                             'special_target1',
-#                             'special_target2',
-#                             'segue1_target1',
-#                             'segue1_target2',
-#                             'segue2_target1',
-#                             'segue2_target2',
-#                             'boss_target1',
-#                             'ancillary_target1',
-#                             'ancillary_target2',
+#                             'special_target1', #
+#                             'special_target2', #
+#                             'segue1_target1', #
+#                             'segue1_target2', #
+#                             'segue2_target1', #
+#                             'segue2_target2', #
+#                             'boss_target1', #
+#                             'ancillary_target1', #
+#                             'ancillary_target2', #
                              'plateID',  # ID
                              'sourceType',  # classification
                              'targetObjID',  # ID
@@ -165,8 +166,8 @@ def load_data():
                              'cModelMagErr_z',
                              'mRrCc_r',
                              'mRrCcErr_r',
-#                             'score',  # quality
-#                             'resolveStatus',  # flag
+#                             'score',  # quality #
+#                             'resolveStatus',  # flag #
 #                             'calibStatus_u',  # magnitude
 #                             'calibStatus_g',  # magnitude
 #                             'calibStatus_r',  # magnitude
@@ -191,7 +192,8 @@ def load_data():
     return data
 
 
-def grid_search_optimizer(data, clf, params, var=None, lda=False):
+def grid_search_optimizer(data, clf, params, var=None, cv=5,
+                          scorer=None):
     X_train, X_test, y_train, y_test = data
 
     if var < 1:
@@ -207,64 +209,71 @@ def grid_search_optimizer(data, clf, params, var=None, lda=False):
         X_train = pca.transform(X_train)
         X_test = pca.transform(X_test)
 
-    if lda:
-        discr = LDA().fit(X_train, y_train)
-
-        X_train = discr.transform(X_train)
-        X_test = discr.transform(X_test)
-
     scaler = preprocessing.StandardScaler().fit(X_train)
     X_train = scaler.transform(X_train)
     X_test = scaler.transform(X_test)
 
-    clf_cv = GridSearchCV(clf, params, cv=5, n_jobs=-1)
+    clf_cv = GridSearchCV(clf, params, cv=cv, n_jobs=-1, scoring=scorer)
     clf_cv.fit(X_train, y_train)
 
     err = 1 - clf_cv.score(X_test, y_test)
     return err, clf_cv.best_params_
 
 
+def count_experiments(clfs):
+    n_exps = np.ones(len(clfs.columns), dtype=np.int16)
+    for i, clf in enumerate(clfs):
+        param_grid = clfs[clf]['grid']
+
+        for param in param_grid:
+            n_exps[i] *= len(param_grid[param])
+
+    return n_exps
+
+
+def custom_score(y, y_pred, pbar=None):
+    matr = confusion_matrix(y, y_pred)
+    if pbar is not None:
+        pbar.update(1)
+
+    return np.sum(np.diagonal(matr)) / np.sum(matr)
+
+
 def analysis(data, tests):
-    # string None b/c pandas indexing. it feels weird tbh
+    n_exps = count_experiments(tests)
+    cv = 5
+
     var_max = 1.
     var_min = .75
     variances = np.linspace(var_min, var_max,
                             num=26)
     variances = np.around(variances, 2)
 
-    lda = [True, False]
-
-#    results = pd.DataFrame(columns=['clf', 'err', 'pca',
-#                                    'lda', 'params'])
     ind = pd.MultiIndex.from_product([(100 * variances).astype(int),
                                       ['err', 'params']],
                                      names=['pca', 'results'])
-    col = pd.MultiIndex.from_product([tests.columns, lda],
-                                     names=['tests', 'lda'])
-    results = pd.DataFrame(index=ind, columns=col)
+    results = pd.DataFrame(index=ind, columns=tests.columns)
 
-    n_tests = len(variances) * len(lda) * len(tests.columns)
+    n_tests = cv * sum(n_exps) * len(variances)
     with tqdm(total=n_tests, file=sys.stdout) as pbar:
-        for test in tests:
+        # TODO: Make this work
+#        scorer = make_scorer(custom_score, pbar=pbar)
+        for i, test in enumerate(tests):
             clf = tests[test]['obj']
             params = tests[test]['grid']
-            for lda_bool in lda:
-                for var in variances:  # variance retained
-                    pbar.set_description("clf: {0}, "
-                                         "lda: {1}, "
-                                         "pca: {2}".format(test,
-                                                           lda_bool,
-                                                           var))
+            for var in variances:  # variance retained
+                pbar.set_description("clf: {0}, "
+                                     "pca: {1}".format(test,
+                                                       var))
 
-                    err, par = grid_search_optimizer(data, clf, params,
-                                                     var=var,
-                                                     lda=lda_bool)
+                err, par = grid_search_optimizer(data, clf, params,
+                                                 var=var,
+                                                 scorer=None)
 
-                    results[test,
-                            lda_bool][int(100 * var), 'err'] = round(err, 4)
-                    results[test, lda_bool][int(100 * var), 'params'] = par
+                results[test][int(100 * var), 'err'] = round(err, 4)
+                results[test][int(100 * var), 'params'] = par
 
-                    pbar.update()
+                pbar.update(cv * n_exps[i])
 
     errs = results.loc[(results.index.get_level_values('results')
                         == 'err')].reset_index(level=1, drop=True)
@@ -293,9 +302,9 @@ def plot_errors(errs):
     plt.figure()
     marker = itertools.cycle(('o', '^', '+'))
 
-    for clf, lda in errs:
-        plt.plot(indicies, errs[(clf, lda)].values,
-                 label="clf: {0}, lda: {1}".format(clf, lda),
+    for clf in errs:
+        plt.plot(indicies, errs[clf].values,
+                 label=clf,
                  marker=next(marker))
 
     plt.ylabel("Error Rate")
@@ -331,26 +340,46 @@ def main():
     clfs = pd.DataFrame(index=['obj', 'grid'])
 
     # then add tests
-    clfs['knn'] = pd.Series([KNeighborsClassifier(),
+    clfs['kNN'] = pd.Series([KNeighborsClassifier(),
                             {'n_neighbors': np.arange(25) + 1,
                              'weights': ['uniform', 'distance']}],
                             index=clfs.index)
-    clfs['lda'] = pd.Series([LDA(), {}], index=clfs.index)
-    clfs['qda'] = pd.Series([QDA(), {}], index=clfs.index)
-#    clfs['svm'] = pd.Series([SVC(),
-#                            {'C': 2. ** np.arange(-6, 5),
-#                             'gamma': 2. ** np.arange(-6, 5),
-#                             'decision_function_shape': ['ovo', 'ovr']}],
-#                            index=clfs.index)
+    clfs['LDA'] = pd.Series([LDA(), {}], index=clfs.index)
+    # QDA Performs extremely poorly without LDA, leaving commented for now
+#    clfs['QDA'] = pd.Series([QDA(), {}], index=clfs.index)
+    clfs['SVM'] = pd.Series([SVC(),
+                            {'C': 2. ** np.arange(-6, 5),
+                             'gamma': 2. ** np.arange(-6, 5),
+                             'decision_function_shape': ['ovo', 'ovr']}],
+                            index=clfs.index)
     sample_range = (2. ** np.arange(1, 11)).astype(int)
-#    clfs['random forest'] = pd.Series([RandomForestClassifier(),
-#                                       {'n_estimators': np.arange(1, 11) * 10,
-#                                        'min_samples_split': sample_range}],
-#                                      index=clfs.index)
-#    clfs['AdaBoost'] = pd.Series([AdaBoostClassifier(),
-#                                  {'n_estimators': np.arange(1, 11) * 10,
-#                                   'learning_rate': 10. ** np.arange(-4, 1)}],
-#                                 index=clfs.index)
+    clfs['Random Forest'] = pd.Series([RandomForestClassifier(),
+                                       {'n_estimators': np.arange(1, 11) * 10,
+                                        'min_samples_split': sample_range}],
+                                      index=clfs.index)
+    clfs['AdaBoost'] = pd.Series([AdaBoostClassifier(),
+                                  {'n_estimators': np.arange(1, 11) * 10,
+                                   'learning_rate': 10. ** np.arange(-4, 1)}],
+                                 index=clfs.index)
+    clfs['MLP'] = pd.Series([MLPClassifier(),
+                             {'hidden_layer_sizes': [(), (2 ** 3, ),
+                                                     (2 ** 4, ), (2 ** 5, ),
+                                                     (2 ** 6, ),
+                                                     (2 ** 3, 2 ** 3),
+                                                     (2 ** 3, 2 ** 4),
+                                                     (2 ** 3, 2 ** 5),
+                                                     (2 ** 3, 2 ** 6),
+                                                     (2 ** 4, 2 ** 4),
+                                                     (2 ** 4, 2 ** 5),
+                                                     (2 ** 4, 2 ** 6),
+                                                     (2 ** 5, 2 ** 5),
+                                                     (2 ** 5, 2 ** 6),
+                                                     (2 ** 5, 2 ** 6)],
+                              'activation': ['identity', 'logistic',
+                                             'tanh', 'relu'],
+                              'learning_rate_init':
+                                  10. ** np.arange(-4, 2)}],
+                            index=clfs.index)
 
     # analyses
     results, errs, pbest = analysis(class_data, clfs)
@@ -359,8 +388,9 @@ def main():
 
     plot_errors(errs)
     print(np.min(errs.values))
+    print(np.min(errs))
 
-    return results, errs, pbest
+    return results, errs, pbest, clfs
 
 
 if __name__ == '__main__':
@@ -373,4 +403,4 @@ if __name__ == '__main__':
     directory = os.path.join(os.path.dirname(__file__), 'out')
     if not os.path.exists(directory):
         os.makedirs(directory)
-    results, errs, pbest = main()
+    results, errs, pbest, clfs = main()
